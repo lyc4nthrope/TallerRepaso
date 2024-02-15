f744e8526a598c3f4793e8b7c337ad6fd412f651
Cristhian Eduardo Osorio Restrepo <osoriocristhian86@gmail.com>
2024-02-15 10:25:59 -0500

    verificacion de campos vacios, eso es en el scenebuilder. (esta en //)

--- src/main/java/co/edu/uniquindio/demo/demo/Main.java
+++ src/main/java/co/edu/uniquindio/demo/demo/Main.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 //ggg
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
@@ -21,3 +22,15 @@ public class Main extends Application {
         launch();
     }
 }
+
+/***
+ *
+ * metodo para verificar si esta todos los campos llenos en la ventana del crud
+ *CrudNarutal natural = new CrudNatural;
+ * if(!txt_nomble.equals("") && !txt_apellido.equals("") &&...){
+ *     personanatural persona = new personanatural(txt_nombre....);
+ *     natural.crearClienteNatu(persona);
+ * }else{
+ *     Joptio.Show.MensaggeDialog(null,"hay campos vacios o no se estan guardando los datos")
+ * }
+ */
