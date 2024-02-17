67332e08c7809faafd6139882b8c0f6e0d9e260b
CamiloAlbertoOspinaAgudelo <148649416+CamiloAlbertoOspinaAgudelo@users.noreply.github.com>
2024-02-17 11:45:20 -0500

    a ver

--- src/main/java/co/edu/uniquindio/demo/demo/HelloController.java
+++ src/main/java/co/edu/uniquindio/demo/demo/HelloController.java
@@ -7,42 +7,5 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private TableColumn<?, ?> jurApellido;
-
-    @FXML
-    private TableColumn<?, ?> jurDireccion;
-
-    @FXML
-    private TableColumn<?, ?> jurID;
-
-    @FXML
-    private TableColumn<?, ?> jurNit;
-
-    @FXML
-    private TableColumn<?, ?> jurNombre;
-
-    @FXML
-    private TableColumn<?, ?> jurTelefono;
-
-    @FXML
-    private TableView<?> listaJuridicos;
-
-    @FXML
-    private TextField textApellido;
-
-    @FXML
-    private TextField textDireccion;
-
-    @FXML
-    private TextField textID;
-
-    @FXML
-    private TextField textNit;
-
-    @FXML
-    private TextField textNombre;
-
-    @FXML
-    private TextField textTelefono;
+    @FXML private ComboBox comboxPais;
 }
