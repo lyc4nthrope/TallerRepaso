2ef4b67f0c24c44a3b8e6c8de4fb9edc720aa072
CamiloAlbertoOspinaAgudelo <148649416+CamiloAlbertoOspinaAgudelo@users.noreply.github.com>
2024-02-16 22:28:35 -0500

    rf
    fh

--- src/main/java/co/edu/uniquindio/demo/demo/HelloController.java
+++ src/main/java/co/edu/uniquindio/demo/demo/HelloController.java
@@ -2,13 +2,47 @@ package co.edu.uniquindio.demo.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TableColumn<?, ?> jurApellido;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TableColumn<?, ?> jurDireccion;
+
+    @FXML
+    private TableColumn<?, ?> jurID;
+
+    @FXML
+    private TableColumn<?, ?> jurNit;
+
+    @FXML
+    private TableColumn<?, ?> jurNombre;
+
+    @FXML
+    private TableColumn<?, ?> jurTelefono;
+
+    @FXML
+    private TableView<?> listaJuridicos;
+
+    @FXML
+    private TextField textApellido;
+
+    @FXML
+    private TextField textDireccion;
+
+    @FXML
+    private TextField textID;
+
+    @FXML
+    private TextField textNit;
+
+    @FXML
+    private TextField textNombre;
+
+    @FXML
+    private TextField textTelefono;
 }
